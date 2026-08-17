71cad060775db12a53fbe34e992060d755a47e9c
deilja <dereilja@gmail.com>
2026-08-18 00:55:39 +0500

    chore: bump XFI Guard version to 1.1.0

--- xfi_guard/__init__.py
+++ xfi_guard/__init__.py
@@ -1,6 +1,6 @@
 """XFI Guard package."""
 
-__version__ = "0.1.0"
+__version__ = "1.1.0"
 
 # Telegram UI: при переходе на новый экран предыдущее меню удаляется.
 from . import menu_manager as _menu_manager  # noqa: F401,E402
